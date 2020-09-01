6f241b040e56e11dec2ebd93d214c5fbf849efa4
AmSmo <adam.smolenski@gmail.com>
2020-08-31 23:08:44 -0400

    fixed bloggers

--- app/controllers/bloggers_controller.rb
+++ app/controllers/bloggers_controller.rb
@@ -3,11 +3,34 @@ class BloggersController < ApplicationController
   before_action :find_blogger, only: [:edit, :update, :show, :destroy]
   
   def new
-
+    if flash[:params]
+      @blogger.update(flash[:params])
+    end
   end
 
   def create
-  
+    @blogger.update(blogger_params)
+    if @blogger.valid?
+      redirect_to blogger_path(@blogger)
+    else 
+      flash[:errors] = @blogger.errors.full_messages
+      flash[:params] = blogger_params
+      redirect_to new_blogger_path
+    end
+
+  end
+
+  def update
+    @blogger.update(blogger_params)
+      
+    if @blogger.valid?
+      redirect_to blogger_path(@blogger)
+    else 
+      flash[:errors] = @blogger.errors.full_messages
+      flash[:params] = blogger_params
+      redirect_to edit_blogger_path(@blogger)
+    end
+
   end
   
   def edit
@@ -20,13 +43,11 @@ class BloggersController < ApplicationController
   def index
   end
 
-  def edit
-  end
-  
-  def update
-  end
-
   private 
+  
+  def blogger_params
+    params.require(:blogger).permit(:name, :bio, :age)
+  end
 
   def new_blogger
     @blogger = Blogger.new
--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,7 +2,11 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
     def featured_post
+      if self.posts != []
         self.posts.order(:likes).last
+      else
+        Blogger.first.posts.first
+      end
     end
 
     
